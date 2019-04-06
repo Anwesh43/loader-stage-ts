ed186d6c63c02e33df1b01576ec3346681ec58df
Anwesh43 <anweshthecool0@gmail.com>
2019-04-06 18:05:48 +0530

    adding State class

--- LoaderStage.ts
+++ LoaderStage.ts
@@ -112,3 +112,25 @@ class LoaderStage {
         stage.handleTap()
     }
 }
+
+class State {
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += ScaleUtil.updateValue(this.scale, this.dir, 1, arcs)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
